3154d0d0b177451fda5f6ce3123a351d4e9d02d7
Robert Murdoch <rmurdoc90@gmail.com>
2020-03-09 13:55:29 -0700

    working

--- js/index.js
+++ js/index.js
@@ -40,3 +40,66 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+let navBar1 = document.querySelector('a:nth-of-type(1)')
+navBar1.textContent = siteContent["nav"]["nav-item-1"]
+
+let navBar2 = document.querySelector('a:nth-of-type(2)')
+navBar2.textContent = siteContent["nav"]["nav-item-2"]
+
+let navBar3 = document.querySelector('a:nth-of-type(3)')
+navBar3.textContent = siteContent["nav"]["nav-item-3"]
+
+let navBar4 = document.querySelector('a:nth-of-type(4)')
+navBar4.textContent = siteContent["nav"]["nav-item-4"]
+
+let navBar5 = document.querySelector('a:nth-of-type(5)')
+navBar5.textContent = siteContent["nav"]["nav-item-5"]
+
+let navBar6 = document.querySelector('a:nth-of-type(6)')
+navBar6.textContent = siteContent["nav"]["nav-item-6"]
+
+// IMAGES
+
+let codeImg1 = document.querySelector('#cta-img')
+codeImg1.setAttribute('src','img/header-img.png')
+
+let codeImg2 = document.querySelector('#middle-img')
+codeImg2.setAttribute('src','img/mid-page-accent.jpg')
+
+// IMAGES END
+
+// CTA SECTION
+
+let titleH1 = document.querySelector('h1')
+titleH1.textContent = siteContent['cta']['h1']
+
+let button = document.querySelector('button')
+button.textContent = siteContent['cta']['button']
+
+// CTA SECTION END
+
+// FEATURE CONTENT
+
+let featureContent = document.querySelector('h4:nth-of-type(1)')
+featureContent.textContent = siteContent['main-content']['features-h4']
+
+let featureContent1 = document.querySelector('p:nth-of-type(1)')
+featureContent1.textContent = siteContent['main-content']['features-content']
+
+// FEATURE CONTENT END
+
+// ABOUT CONTENT
+
+let featureAbout = document.querySelector('h4:nth-of-type(2)')
+featureAbout.textContent = siteContent['main-content']['about-h4']
+
+let featureAbout1 = document.querySelector('p:nth-of-type(2)')
+featureAbout1.textContent = siteContent['main-content']['about-content']
+
+// ABOUT CONTENT END
+
+
+
+
